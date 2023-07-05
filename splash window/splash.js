2c149bfdea9fd1edfad3de0603d45e295cc38863
Dinujaya Manodya <dinujayamanodya@gmail.com>
2023-07-05 10:58:46 +0530

    done

--- splash window/splash.js
+++ splash window/splash.js
@@ -1,12 +1,22 @@
 function removeSplashScreen() {
     var splash = document.querySelector('.splash');
     splash.parentNode.removeChild(splash);
-    window.location.replace("cart.html");
+    window.location.href = "../home/index.html";
   }
-
 setTimeout(removeSplashScreen, 4000);
 
 
   // setTimeout(function() {
     
   // }, 1000);
+
+// function transitionToMainPage() {
+//   setTimeout(function() {
+//     window.location.href = "../home/index.html";
+//   },4000); 
+// }
+
+
+// window.onload = function() {
+//   transitionToMainPage();
+// };
