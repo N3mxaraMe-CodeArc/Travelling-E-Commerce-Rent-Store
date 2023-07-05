c76760431fa6ef93ddc80e5f0a8d4b1d6fb64b4a
N3mxara <nimsara.20223294@iit.ac.lk>
2023-07-05 11:11:52 +0530

    git push -u origin main

--- splash window/splash.js
+++ splash window/splash.js
@@ -1,12 +1,9 @@
 function removeSplashScreen() {
-    var splash = document.querySelector('.splash');
+  var splash = document.querySelector('.splash');
+  if (splash) {
     splash.parentNode.removeChild(splash);
-    window.location.replace("cart.html");
   }
+  window.location.href = "../home/index.html";
+}
 
 setTimeout(removeSplashScreen, 4000);
-
-
-  // setTimeout(function() {
-    
-  // }, 1000);
