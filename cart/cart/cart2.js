d510f524a95065b5d19163cd4c6213991c63b603
Dinujaya Manodya <dinujayamanodya@gmail.com>
2023-07-06 23:28:30 +0530

    jgvg

--- cart/cart/cart2.js
+++ cart/cart/cart2.js
@@ -1,3 +1,19 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
 function addtocart(){
     var item = document.createElement('p');
     item.classList.add("cart");    
