8f43f270e8f9fa7254258b9daf1eda9930850aa4
Dinujaya Manodya <dinujayamanodya@gmail.com>
2023-07-08 11:51:52 +0530

    hf.f

--- cart/cart/shop.js
+++ cart/cart/shop.js
@@ -15,8 +15,21 @@ closeShopping.addEventListener('click', ()=>{
 })
 
 function addtocart(){
-    var item = document.createElement('p');
-    item.classList.add("cart");    
+    var item = document.createElement('div');
+    item.classList.add("cart-add");    
     var cartItem = document.getElementById("cart-item");
-    var proName = cartItem.children[0].innerHTML;    
+    var proName = cartItem.children[0].innerHTML;  
+    var image = document.createElement('img'); 
+    image.src = proImg; 
+    var proImg = cartItem.children[1].src;
+    var proPrz = cartItem.children[2].innerHTML;
+
+    item.append(image);
+    item.append(proName);
+    item.append(proPrz);
+
+    document.getElementById("itemContent").append(item);
+
+    console.log(document.getElementById("itemContent"));
+0
 }
