b0a66894c268a542e4c0ae2cfb12f821aa286829
N3mxara <nimsara.20223294@iit.ac.lk>
2023-07-22 09:02:40 +0530

    push

--- Feedback Form/script.js
+++ Feedback Form/script.js
@@ -1,9 +1,19 @@
 let popup = document.getElementById("popup");
 
 function openPopup(){
-   popup.classList.add("open-popup");
+   
 }
 
 function closePopup(){
    popup.classList.remove("open-popup");
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+   popup.classList.add("open-popup");
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+   popup.classList.add("open-popup");
+   popup.classList.remove("open-popup");
+   window.location.href("../Home2/index.html")
+})
