c6e64c0dd35b82b38ddc7d5b0872622177f4d3dd
N3mxara <nimsara.20223294@iit.ac.lk>
2023-07-23 18:33:32 +0530

    kk

--- Feedback Form/script.js
+++ Feedback Form/script.js
@@ -1,41 +1,56 @@
-var btnsubmit = document.getElementById("button-submit-feedback")[0];
-btnsubmit.onclick = displayPopup
+// var btnsubmit = document.getElementById("button-submit-feedback")[0];
+// btnsubmit.onclick = displayPopup
 
-function displayPopup(){
-   document.getElementById("section").classList.remove("popup-section")
-}
+// function displayPopup(){
+//    document.getElementById("section").classList.remove("popup-section")
+// }
 
 // const clicksubmit = () => {
 //    document.getElementsByClassName("popup-section")[0].classList.add("popup-section-after")let 
 
 
-var firstName = document.getElementById("firstName").value;
-var lastName = document.getElementById("lastName").value;
-var email = document.getElementById("email").value;
-
-function nameValid(name){
-   nameCharacters = /^[A-Za-z]+$/;
-   return nameCharacters.test(name);
-}
-function emailValid(email){
-   emailCharacters = /^[@.]+$/;
-   return emailCharacters.test(email);
-}
 
 
 
 
 const SubmitHandler = () => {
-   if(!nameValid(firstName)){
-      document.getElementById("name-validation-alert").innerHTML="Enter the Valid Name"
+
+   var firstName = document.getElementById("firstName").value;
+   var lastName = document.getElementById("lastName").value;
+   var email = document.getElementById("email").value;
+   var num = document.getElementById("phoneNum").value;
+
+   function nameValid(name) {
+      nameCharacters = /^[A-Za-z]+$/;
+      return nameCharacters.test(name);
    }
-   if(!nameValid(lastName)){
-      document.getElementById("name-validation-alert").innerHTML="Enter the Valid Name"
+   function emailValid(email) {
+      emailCharacters = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+      return emailCharacters.test(email);
    }
-   if(!emailValid(email)){
-      document.getElementById("name-validation-alert").innerHTML="Enter the Email"
+
+   function numberValid(num){
+      numCharacters = /^[0-9]+$/;
+      return numCharacters.test(num);
+   }
+
+   if (!nameValid(firstName)) {
+      document.getElementById("name-validation-alert").innerHTML = "Enter the Valid Name"
+      document.getElementById("firstName").style.border = "1px solid red";
+   }
+   else if(!nameValid(lastName)){
+      document.getElementById("name-validation-alert").innerHTML = "Enter the Valid Name"
+      document.getElementById("lastName").style.border = "1px solid red";
+   }
+   else if(!emailValid(email)){
+      document.getElementById("name-validation-alert").innerHTML = "Enter the Valid Email"
+      document.getElementById("email").style.border = "1px solid red";
+   }
+   else if(numberValid(num) && num.length != 10){
+      document.getElementById("name-validation-alert").innerHTML = "Enter the Valid Contact"
+      document.getElementById("phoneNum").style.border = "1px solid red";
    }
    else{
-      window.location.href = "../Feedback Form/popupbox.html"
+      document.getElementById("name-validation-alert").innerHTML = ""
    }
 }
