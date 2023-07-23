15e525eec40a9afcc2e1bc45d4b844aa5443c177
N3mxara <nimsara.20223294@iit.ac.lk>
2023-07-23 10:38:01 +0530

    hh

--- Feedback Form/script.js
+++ Feedback Form/script.js
@@ -1,19 +1,41 @@
-let popup = document.getElementById("popup");
+var btnsubmit = document.getElementById("button-submit-feedback")[0];
+btnsubmit.onclick = displayPopup
 
-function openPopup(){
-   
+function displayPopup(){
+   document.getElementById("section").classList.remove("popup-section")
 }
 
-function closePopup(){
-   popup.classList.remove("open-popup");
+// const clicksubmit = () => {
+//    document.getElementsByClassName("popup-section")[0].classList.add("popup-section-after")let 
+
+
+var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var email = document.getElementById("email").value;
+
+function nameValid(name){
+   nameCharacters = /^[A-Za-z]+$/;
+   return nameCharacters.test(name);
+}
+function emailValid(email){
+   emailCharacters = /^[@.]+$/;
+   return emailCharacters.test(email);
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-   popup.classList.add("open-popup");
-})
 
-window.addEventListener("DOMContentLoaded", () => {
-   popup.classList.add("open-popup");
-   popup.classList.remove("open-popup");
-   window.location.href("../Home2/index.html")
-})
+
+
+const SubmitHandler = () => {
+   if(!nameValid(firstName)){
+      document.getElementById("name-validation-alert").innerHTML="Enter the Valid Name"
+   }
+   if(!nameValid(lastName)){
+      document.getElementById("name-validation-alert").innerHTML="Enter the Valid Name"
+   }
+   if(!emailValid(email)){
+      document.getElementById("name-validation-alert").innerHTML="Enter the Email"
+   }
+   else{
+      window.location.href = "../Feedback Form/popupbox.html"
+   }
+}
